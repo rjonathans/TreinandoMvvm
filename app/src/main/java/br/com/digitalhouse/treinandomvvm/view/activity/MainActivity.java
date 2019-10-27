ff4a5acc2be14251814767bb65da12a0863f21ca
rjonathans <rjonath@live.com>
2019-10-26 21:54:44 -0300

    criação do XML da Main

--- app/src/main/java/br/com/digitalhouse/treinandomvvm/view/activity/MainActivity.java
+++ app/src/main/java/br/com/digitalhouse/treinandomvvm/view/activity/MainActivity.java
@@ -1,10 +1,19 @@
-package br.com.digitalhouse.treinandomvvm;
+package br.com.digitalhouse.treinandomvvm.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import br.com.digitalhouse.treinandomvvm.R;
+
+//Todo: 1º passo: criei a classe view.main.activity.class
+
+
 public class MainActivity extends AppCompatActivity {
+
+//Todo: 3º passo: declare o recycler view
+    private RecyclerView recyclerViewMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
